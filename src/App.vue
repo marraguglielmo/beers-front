192a1970a37f6f5a5258472dc8bc07f1b1d04e39
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-05-31 12:54:09 +0200

    [add] apiUrl

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import axios from 'axios';
+import {store} from './data/store'
+
+  export default {
+    data(){
+      return{
+        beers: [],
+      }
+    },
+
+    methods:{
+      getApi(){
+        axios.get(store.apiUrl)
+          .then(result =>{
+            console.log(result.data);
+            this.beers = result.data
+          })
+          .catch(error =>{
+            console.log(error.message);
+          })
+      }
+    },
+
+    mounted(){
+        this.getApi();
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <h2>Lista birre</h2>
+      <ul>
+        <li v-for="beer in beers" :key="beer.id">{{beer.id}} - {{beer.name}}</li>
+      </ul>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
